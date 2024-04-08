7619db46a3ed3d153bba2fbf11be05b13a8be2a9
Palmatt98 <palmierimatteo98@gmail.com>
2024-04-08 17:48:15 +0200

    milestone 2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+let operatoreSelezionato = null;
+let primoOperando = null;
+
 const buttonsArray = document.querySelector(".numbers").children;
 // elemento.children mi restituisce un array  contenente i figli di elelemento
 console.log(buttonsArray);
@@ -9,9 +12,25 @@ for (let i = 0; i < buttonsArray.length; i++) {
 		const resultELement = document.getElementById("result");
 		console.log(resultELement.innerText);
 		console.log(event.target.innerText);
-		if (resultELement.innerText === "0"){
-			resultELement.innerText = ""
+		if (resultELement.innerText === "0") {
+			resultELement.innerText = "";
 		}
 		resultELement.innerText = resultELement.innerText + event.target.innerText;
+	});
+}
+
+const operatori = document.querySelector(".operators").children;
+console.log(operatori);
+
+for (let i = 0; i < operatori.length; i++) {
+	operatori[i].addEventListener("click", function (event) {
+		operatoreSelezionato = event.target.innerText;
+		resultELement = document.getElementById("result");
+		primoOperando = resultELement.innerText;
+
+		resultELement.innerText = "0";
+
+		console.log("primoOperando: ", primoOperando);
+		console.log("operatore selezionato: ", operatoreSelezionato);
 	});
 }
